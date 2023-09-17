5a7b674959546b8f72a4b4c4339fb2639cf0061d
Sprevon <sevenuniversity@outlook.com>
2023-09-17 13:15:14 +0800

    feat: 第三章作业的第一种实现

---
+++ Homework3/src/main/java/BeerServlet.java
@@ -0,0 +1,28 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @Comments
+ * @Author LeonBwChen
+ * @Date 2023/9/17 12:45
+ */
+@WebServlet(name = "BeerServlet", value = "/selectBeer")
+public class BeerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("Beer Selection Advice<br>");
+        printWriter.println("<br>Got beer color " + req.getParameter("color"));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-20230913-MVC小测：Quiz1
 
+20230913-MVC小测：Quiz1 <br><br>
+20230919-陈博文-第三章作业
+<hr>
 Application content设置: /WebCourseQuiz
 
 Tomcat版本: 8.5.85
+
+
